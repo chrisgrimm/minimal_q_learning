0efaf9f868c820fc153a9eeb0ff1444b324a9fa1
Chris Grimm <cgrimm1994@gmail.com>
2018-09-02 23:42:08 -0400

    getting tired...accidentally ran the original 3 ship test with 2 partitions. rerunning

--- visualization.py
+++ visualization.py
@@ -102,7 +102,7 @@ def produce_assault_reward_visualization(network, env, name):
         obs_samples = pickle.load(f)
     all_partitioned_rewards = []
     for ship_i_examples in obs_samples:
-        average_reward = np.mean(network.get_partitioned_reward(ship_i_examples[:10]), axis=0, keepdims=True)
+        average_reward = np.mean(network.get_partitioned_reward(ship_i_examples[:10], [1]*10), axis=0, keepdims=True)
         all_partitioned_rewards.append(average_reward)
     all_partitioned_rewards = np.concatenate(all_partitioned_rewards, axis=0)
     all_partitioned_rewards = (255*all_partitioned_rewards).astype(np.uint8)
