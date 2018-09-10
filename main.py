63e0865cb1dca3a46a2dd2dcd82675a9fd8318af
Chris Grimm <cgrimm1994@gmail.com>
2018-09-10 16:56:20 -0400

    changed number of reward pretraining steps to 10000 for assault. moved incrementing value outside of loop

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ print(env.action_space)
 epsilon = 1.0
 min_epsilon = 0.1
 num_epsilon_steps = 100000
-num_reward_steps = 1000
+num_reward_steps = 10000
 current_reward_training_step = 0 if args.separate_reward_repr else num_reward_steps
 epsilon_delta = (epsilon - min_epsilon) / num_epsilon_steps
 i = 0
