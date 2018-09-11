8044f4b52b713b63385ac4f789420b1374a91aa8
Chris Grimm <cgrimm1994@gmail.com>
2018-09-11 13:48:55 -0400

    added an extra convolution

--- reward_network.py
+++ reward_network.py
@@ -306,7 +306,8 @@ class RewardPartitionNetwork(object):
             Y = Y.astype(np.float32)
             X = tf.reshape(X, [1, 64, 64])
             Y = tf.reshape(Y, [1, 64, 64])
-            detectors = tf.layers.conv2d(x, 2*num_objects, 4, 1, padding='SAME')
+            detectors = tf.layers.conv2d(x, 32, 4, 1, padding='SAME', name='d1')
+            detectors = tf.layers.conv2d(detectors, 2*num_objects, 4, 1, padding='SAME', name='d2')
             coords = []
             for i in range(num_objects):
                 object_slice = detectors[:, :, :, 2*i:2*(i+1)]
