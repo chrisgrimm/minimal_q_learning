5d50b34d8a143cac71a209e08bd3f3849edd2b46
Chris Grimm <cgrimm1994@gmail.com>
2018-10-11 14:44:00 -0400

    switched sigmoid for tanh

--- reward_network.py
+++ reward_network.py
@@ -340,7 +340,7 @@ class RewardPartitionNetwork(object):
                 x = tf.layers.conv2d(x, 64, 4, 2, 'SAME', activation=tf.nn.relu, name='c1')  # [bs, 8, 8, 64]
                 x = tf.layers.conv2d(x, 64, 3, 1, 'SAME', activation=tf.nn.relu, name='c2')  # [bs, 8, 8, 64]
                 x = tf.layers.dense(tf.reshape(x, [-1, 8 * 8 * 64]), 128, activation=tf.nn.relu, name='fc1')
-                soft = tf.nn.softmax(tf.layers.dense(x, len(self.Q_networks), activation=tf.nn.sigmoid, name='qa'))
+                soft = tf.nn.softmax(tf.layers.dense(x, len(self.Q_networks), activation=tf.nn.tanh, name='qa'))
         rewards = tf.reshape(r, [-1, 1]) * soft #* error_control
         return rewards
 
