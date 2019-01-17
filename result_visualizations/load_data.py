b3ad54d47b096bd1ddc81ad03ad022a4994c427c
Christopher Ryan Grimm <crgrimm@rldl11.eecs.umich.edu>
2019-01-17 15:17:11 -0500

    merging load data changes

--- result_visualizations/load_data.py
+++ result_visualizations/load_data.py
@@ -410,10 +410,10 @@ def make_repeat_plots(run_dir, dest_dir):
 
 def make_meta_controller_plots(path, meta_runs, baseline_runs, n=1, y_range=None, meta_to_baseline_mapping=None):
     all_keys = set(list(meta_runs.keys()))
-    resolution = 10000
+    resolution = 1000
     for game in all_keys:
         plt.clf()
-        plt.hold(True)
+        #plt.hold(True)
         # baselines
         all_ys = []
         bl_game = game if meta_to_baseline_mapping is None else meta_to_baseline_mapping[game]
@@ -421,6 +421,7 @@ def make_meta_controller_plots(path, meta_runs, baseline_runs, n=1, y_range=None
             print(len(data))
             print(len(data['cum_reward']))
             x = [time for time, J in data['cum_reward'] if time % resolution == 0][1:]
+            print('last time', x[-1])
             y = smooth([J for time, J in data['cum_reward'] if time % resolution == 0][1:], weight=0.95)
             print('shapes', len(data['cum_reward']), np.shape(y))
             all_ys.append(y)
@@ -440,8 +441,9 @@ def make_meta_controller_plots(path, meta_runs, baseline_runs, n=1, y_range=None
         for reward, data_runs in meta_runs[game].items():
             all_ys = []
             for i, data in enumerate(data_runs):
-                x = [time for time, J in data['cum_reward'] if time % resolution == 0][1:]
+                #x = [time for time, J in data['cum_reward'] if time % resolution == 0][1:]
                 y = smooth([J for time, J in data['cum_reward'] if time % resolution == 0][1:], weight=0.95)
+                print('last time', x[-1])
                 all_ys.append(y)
                 print('meta_run', i, reward, np.shape(y))
                 #plt.plot(x, y, color=color_mapping[reward], label=f'{reward} rewards')
@@ -456,7 +458,7 @@ def make_meta_controller_plots(path, meta_runs, baseline_runs, n=1, y_range=None
             plt.ylabel('Score')
             plt.fill_between(x, mean-err, mean+err, color=color_mapping[reward], alpha=0.5)
         plt.legend()
-        plt.hold(False)
+        #plt.hold(False)
         plt.savefig(os.path.join(path, f'{game}_meta.pdf'))
 
 
@@ -483,8 +485,8 @@ if __name__ == '__main__':
     meta_runs, baseline_runs = load_metacontroller_and_baseline_data(
       '/home/crgrimm/minimal_q_learning/ALL_DATA/cut_down_baselines',
        r'^baseline\_(.+)\_(\d+)$',
-      '/home/crgrimm/minimal_q_learning/ALL_DATA/cut_down_meta',
-       r'^meta\_(.+)\_(\d+)reward\_10mult\_(\d+)$',
+      '/home/crgrimm/minimal_q_learning/ALL_DATA/cut_down_icf_comparison_repeat',
+       r'^(.+)\_(\d+)reward\_1repeat\_(\d+)$',
       meta_name='seaquest',
       baseline_name='seaquest')
     make_meta_controller_plots(
