22046d2213233cfd5a2d2d018bc976c037f87df3
Chris Grimm <cgrimm1994@gmail.com>
2019-03-26 11:17:01 -0400

    trying version of code where constraints are randomly selected to be enforced at each timestep. hopefully this improves speed.

--- reward_network2.py
+++ reward_network2.py
@@ -84,7 +84,7 @@ class ReparameterizedRewardNetwork(object):
 
 
     def train_R_functions(self, time):
-        q_loss = self.train_Q_functions(time)
+        #q_loss = self.train_Q_functions(time)
         S, A, R, SP, T = self.buffer.sample(self.batch_size)
         [_, sums_to_R, greater_than_0, reward_consistency, J_indep, J_nontriv] = self.sess.run(
             [self.train_op, self.sums_to_R, self.greater_than_0, self.reward_consistency, self.J_indep, self.J_nontriv],
